0aebc6a6923a6cd07dbc56855d4dafb89c50d180
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-12-14 01:21:58 +0800

    Zipper +initZipper & export

--- src/Data/Tree/AugmentedRBTree/Zipper.hs
+++ src/Data/Tree/AugmentedRBTree/Zipper.hs
@@ -1,5 +1,18 @@
 module Data.Tree.AugmentedRBTree.Zipper
   ( Zipper (Zipper)
+  , initZipper
+  , partialUpZipper
+  , upZipper
+  , partialDownLeftZipper
+  , downLeftZipper
+  , partialDownRightZipper
+  , downRightZipper
+  , backLastLeftZipper
+  , backLastRightZipper
+  , mostDownLeftBranchZipper
+  , mostDownRightBranchZipper
+  , nextBranchZipper
+  , prevBranchZipper
   ) where
 
 import Data.Tree.AugmentedRBTree.Tree
@@ -7,6 +20,10 @@ import Data.Tree.AugmentedRBTree.Tree
 data Dir = GoLeft | GoRight
 data Step v a = Step !Dir (Tree v a)
 data Zipper v a = Zipper (Tree v a) ![Step v a]
+
+-- | Get a zipper on the root node
+initZipper :: Tree v a -> Zipper v a
+initZipper t = Zipper t []
 
 -- | Move the zipper up, without checking
 partialUpZipper :: Zipper v a -> Zipper v a
