767fbefbcfffb227da4de912402d5df50bf3ddcc
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-12-15 23:10:17 +0800

    Tree/Check

---
+++ src/Data/Tree/AugmentedRBTree/Tree/Check.hs
@@ -0,0 +1,71 @@
+module Data.Tree.AugmentedRBTree.Tree.Check where
+
+import Data.Tree.AugmentedRBTree.Tree
+import Data.Tree.AugmentedRBTree.Augment (Augment)
+
+isRootBlack :: Tree v a -> Bool
+isRootBlack t = color t == black
+
+-- | Count the number of black nodes from root to leave.
+--   return Nothing if there's any path with different count.
+balancedBlackCount :: Tree v a -> Maybe Int
+balancedBlackCount Leave = Just 1
+balancedBlackCount (Branch c _ _ l r) =
+  case balancedBlackCount l of
+    Nothing -> Nothing
+    Just lCount -> case balancedBlackCount r of
+      Nothing -> Nothing
+      Just rCount -> if lCount /= rCount
+        then Nothing
+        else if c == black
+          then Just $! lCount + 1
+          else Just lCount
+
+isBalanced :: Tree v a -> Bool
+isBalanced t = case balancedBlackCount t of
+  Just _ -> True
+  Nothing -> False
+
+-- | Check if there are no nearby red nodes.
+isRedValid :: Tree v a -> Bool
+isRedValid Leave = True
+isRedValid (Branch c _ _ l r) =
+  isRedValid l && isRedValid r && 
+  ( c == black || color l == black && color r == black )
+
+valueRange :: Ord a => Tree v a -> Maybe (Maybe (a, a))
+valueRange Leave = Just Nothing
+valueRange (Branch _ _ a l r) = result where
+  (lGood, min) = case valueRange l of
+    Nothing -> (False, undefined)
+    Just lRange -> case lRange of
+      Nothing -> (True, a)
+      Just (lMin, lMax) -> (lMax <= a, lMin)
+  (rGood, max) = case valueRange r of
+    Nothing -> (False, undefined)
+    Just rRange -> case rRange of
+      Nothing -> (True, a)
+      Just (rMin, rMax) -> (a <= rMin, rMax)
+  result = if lGood && rGood
+    then Just (Just (min, max))
+    else Nothing
+
+-- | Check node value order
+isOrdered :: Ord a => Tree v a -> Bool
+isOrdered t = case valueRange t of
+  Nothing -> False
+  Just _ -> True
+
+isAugmentValid :: (Eq v, Augment v a) => Tree v a -> Bool
+isAugmentValid Leave = True
+isAugmentValid (Branch _ v a l r) =
+  isAugmentValid l && isAugmentValid r &&
+  buildAugment l a r == v
+
+isValid :: (Eq v, Ord a, Augment v a) => Tree v a -> Bool
+isValid t =
+  isOrdered t &&
+  isAugmentValid t &&
+  isBalanced t &&
+  isRootBlack t &&
+  isRedValid t
