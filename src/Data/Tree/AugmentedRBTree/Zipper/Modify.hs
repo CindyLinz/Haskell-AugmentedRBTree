c8fcd5d22501d68bb714e2ec4fab9fc107e7e783
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-12-20 02:59:51 +0800

    Zipper.Modify deleteZipper (not tested)

--- src/Data/Tree/AugmentedRBTree/Zipper/Modify.hs
+++ src/Data/Tree/AugmentedRBTree/Zipper/Modify.hs
@@ -60,13 +60,74 @@ insertZipper a (Zipper _ ss) = insertFix (Zipper (branch red a leave leave) ss)
 --   The returned zipper will point to a starting rotation node.
 deleteZipper :: Augment v a => Zipper v a -> Zipper v a
 deleteZipper z@(Zipper (Branch c v a l r) ss) = case (l, r) of
-  (Leave, r) -> if c == red then o else fixDelete o where o = Zipper r ss
-  (l, Leave) -> if c == red then o else fixDelete o where o = Zipper l ss
+  (Leave, Leave) -> if c == red then Zipper Leave ss else deleteFix (Zipper Leave ss)
+  (Leave, Branch _ v a _ _) -> Zipper (Branch black v a Leave Leave) ss
+  (Branch _ v a _ _, Leave) -> Zipper (Branch black v a Leave Leave) ss
   (_, _) -> deleteZipper victim
     where
       victim = mostDownLeftBranchZipper (partialDownRightZipper (Zipper (branch c victimA l r) ss))
       Zipper (Branch _ _ victimA _ _) _ = victim
 
   where
-    fixDelete :: Augment v a => Zipper v a -> Zipper v a
-    fixDelete = undefined
+    deleteFix :: Augment v a => Zipper v a -> Zipper v a
+    deleteFix nZ =
+      let
+        Zipper n@(Branch nC nV nA nL nR) nS = nZ
+        pZ@(Zipper ~p@(Branch pC pV pA pL pR) pS) = partialUpZipper nZ
+        Step nD _ : _ = nS
+        s = if nD == dirLeft then pR else pL
+        sC = color s
+        Branch _ sV sA sL sR = s
+      in case nS of
+        [] -> nZ -- case 1
+        _ -> if sC == red
+          then -- case 2
+            let
+              (pL', pR', sL', sR') = if nD == dirLeft
+                then (undefined, sL, undefined, sR)
+                else (sR, undefined, sL, undefined)
+            in tryCase3 $ Zipper n (Step nD (branch red pA pL' pR') : Step nD (branch black sA sL' sR') : pS)
+          else
+            tryCase3 nZ
+          where
+            tryCase3 nZ =
+              let
+                Zipper n@(Branch nC nV nA nL nR) nS = nZ
+                pZ@(Zipper p@(Branch pC pV pA pL pR) pS) = partialUpZipper nZ
+                s@(Branch sC sV sA sL sR) = if nD == dirLeft then pR else pL
+              in if color sL == black && color sR == black
+                then
+                  let
+                    (pL', pR') = if nD == dirLeft
+                      then (n, s')
+                      else (s', n)
+                    s' = Branch red sV sA sL sR
+                  in if pC == black && color sL == black && color sR == black
+                    then -- case 3
+                      deleteFix $ Zipper (Branch pC pV pA pL' pR') pS
+                    else -- case 4
+                      Zipper (Branch black pV pA pL' pR') pS
+                else
+                  let
+                    (pL', pR') = if nD == dirLeft
+                      then (n, branch black sLA sLL (branch red sA sLR sR))
+                      else (branch black sRA sRR (branch red sA sRL sL), n)
+                    Branch sLC sLV sLA sLL sLR = sL
+                    Branch sRC sRV sRA sRL sRR = sR
+                  in if nD == dirLeft && color sR == black || nD == dirRight && color sL == black
+                    then -- case 5
+                      doCase6 $ Zipper n (Step nD (branch pC pA pL' pR') : pS)
+                    else
+                      doCase6 nZ
+              where
+                doCase6 nZ = Zipper (branch pC sA sL' sR') pS -- case 6
+                  where
+                    Zipper n@(Branch nC nV nA nL nR) nS = nZ
+                    pZ@(Zipper p@(Branch pC pV pA pL pR) pS) = partialUpZipper nZ
+                    s@(Branch sC sV sA sL sR) = if nD == dirLeft then pR else pL
+                    Branch sLC sLV sLA sLL sLR = sL
+                    Branch sRC sRV sRA sRL sRR = sR
+                    (sL', sR') = if nD == dirLeft
+                      then (branch black pA n sL, Branch black sRV sRA sRL sRR)
+                      else (Branch black sLV sLA sLL sLR, branch black pA sR n)
+
